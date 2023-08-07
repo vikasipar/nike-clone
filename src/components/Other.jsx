5225930753e4e9f5b0c98322be2c83cf4d12f0f1
vikasipar <vickyipar01@gmail.com>
2023-08-07 17:30:11 +0530

    feat-scrollTo(top)

--- src/components/Other.jsx
+++ src/components/Other.jsx
@@ -4,13 +4,19 @@ function Other({heroProduct, data, heroHandler}) {
 
     const datalist = data.filter((product) => product.id !== heroProduct.id );
 
+    const clickHandler = (product) => {
+        heroHandler(product);
+
+        window.scrollTo({top: 0, behavior: "smooth"});
+    }
+
     return (
     <div>
         <h1 className='text-xl font-semibold mx-28 mt-36'>You Might Also Like</h1>
 
         <div className='flex mx-28 my-4 items-center gap-6 mb-24'>
             {datalist.map((product) => (
-                <div key={product.id} className='cursor-pointer' onClick={() => heroHandler(product)}>
+                <div key={product.id} className='cursor-pointer' onClick={() => clickHandler(product)}>
                     <div>
                         <img src={product.images[0]} alt={product.name} className='w-82' />
                     </div>
